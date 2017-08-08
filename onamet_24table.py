5a66599864fbf65187d7d10f3c5b5ce640001883
Unknown <fpichardom@yahoo.com>
2017-08-07 22:36:04 -0230

    Better fix exception handling

--- onamet_24table.py
+++ onamet_24table.py
@@ -9,20 +9,35 @@ def create_record(lista):
         'estacion':lista[0].strip(),
         'plataforma':lista[1].strip(),
         'datetime': set_datetime(lista[2].strip(), lista[3].strip()),
-        'presion_atmosferica':float(lista[4]),
-        'fuerza_viento':float(lista[5]),
-        'direccion_viento':int(lista[6]),
-        'temperatura':float(lista[7]),
-        'lluvia':float(lista[8]),
-        'lluvia_24h':float(lista[9])
+        'presion_atmosferica':check_float(lista[4]),
+        'fuerza_viento':check_float(lista[5]),
+        'direccion_viento':check_int(lista[6]),
+        'temperatura':check_float(lista[7]),
+        'lluvia':check_float(lista[8]),
+        'lluvia_24h':check_float(lista[9])
     }
     return p_record
 
 def set_datetime(date, time):
-    date = [int(i) for i in date.split('-')]
-    time = [int(i) for i in time.split(':')]
-    complete = datetime.datetime(date[2], date[1], date[0], time[0], time[1], time[2])
-    return complete
+    try:
+        date = [int(i) for i in date.split('-')]
+        time = [int(i) for i in time.split(':')]
+        complete = datetime.datetime(date[2], date[1], date[0], time[0], time[1], time[2])
+        return complete
+    except (ValueError,IndexError):
+        return date+' '+time
+
+def check_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+    
+def check_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
 
 def parse_json(url):
     request = requests.get(url)
@@ -34,7 +49,14 @@ def database_connection(uri, database, collection):
     database = client[database]
     collection = database[collection]
     return collection
-
+def write_result(result='success',error=''):
+    if result == 'success':
+        with open(os.path.join(os.path.expanduser('~'),'onamet24.log'), 'a') as outFile:
+            outFile.write('Records write success:' + str(datetime.datetime.utcnow())+'\n')
+    elif result == 'fail':
+        with open(os.path.join(os.path.expanduser('~'),'onamet24.log'), 'a') as outFile:
+            outFile.write('Records write failure:' + str(datetime.datetime.utcnow())+':Error-'+str(error)+'\n')
+            
 if __name__ == "__main__":
 #records = [create_record(i) for i in json['aaData']]
     URI = 'mongodb://localhost:27017/'
@@ -46,9 +68,6 @@ if __name__ == "__main__":
         CONN = database_connection(URI, DB, COL)
         for item in JSON['aaData']:
             CONN.insert_one(create_record(item))
-        with open(os.path.join(os.path.expanduser('~'),'onamet24.log'), 'a') as outFile:
-            outFile.write('Records write success:' + str(datetime.datetime.utcnow())+'\n')
-    except:
-        with open(os.path.join(os.path.expanduser('~'),'onamet24.log')
-, 'a') as outFile:
-            outFile.write('Records write failure:' + str(datetime.datetime.utcnow())+'\n')
+        write_result()
+    except ValueError as e:
+        write_result('fail',e)
